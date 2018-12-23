1620230b6b3b82eeab81d074f04229a4adc3ac9a
League <league@WTS9.attlocal.net>
2018-12-23 11:31:18 -0800

    (IP)
    work on advanced robots and thread pool, ask about robots

--- src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
+++ src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
@@ -1,6 +1,27 @@
 package _02_Advanced_Robot_Race;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+
 public class AdvancedRobotRace {
+
 	// Re-do the robot race recipe from level 3 module 0. 
 	// This time, use threads to make all of the robots go at the same time.
+	public static void main(String[] args) {
+		Robot[] robots = new Robot[5];
+		for (int i = 0; i < robots.length; i++) {
+			try {
+				robots[i]=new Robot();
+				robots[i].
+				
+			} catch (AWTException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	public void Race(Robot r) {
+	
+		
+	}
 }
--- src/_04_Thread_Pool/Worker.java
+++ src/_04_Thread_Pool/Worker.java
@@ -1,11 +1,19 @@
 package _04_Thread_Pool;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class Worker implements Runnable {
-
+	ConcurrentLinkedQueue<Task> taskQueue;
 	@Override
+	public Worker() {
+		taskQueue= new ConcurrentLinkedQueue<Task>();
+	}
 	public void run() {
 		// TODO Auto-generated method stub
+		if(taskQueue.size()!=0) {
+			taskQueue.remove().perform();
+		
+		}
 		
 	}
 
